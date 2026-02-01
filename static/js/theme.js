86e7ea4482bfcc7803582e2039e648ce47421366
Andy Levy <andy@flxsql.com>
2026-02-01 14:42:40 -0500

    visible anchor links to subheadings

--- static/js/theme.js
+++ static/js/theme.js
@@ -17,4 +17,11 @@
   if(saved) apply(saved);
 
   if(btn) btn.addEventListener('click', toggle);
+
+  // Make subheadings clickable anchors
+  document.querySelectorAll('.post-content h2[id], .post-content h3[id], .post-content h4[id]').forEach(heading => {
+    heading.addEventListener('click', () => {
+      window.location.hash = heading.id;
+    });
+  });
 })();
